c5b6ffbd4451e23a498e30ed7be2635709a7de33
LTruijens_cp <SND\LTruijens_cp>
2013-03-09 23:50:30 +0000

    Refactor to make code more maintainable

---
+++ PowerShellGac/PowerShellGac/AssemblyNameExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace PowerShellGac
+{
+    public static class AssemblyNameExtensions
+    {
+        public static string GetFullyQualifiedName(this AssemblyName assemblyName)
+        {
+            if (assemblyName.ProcessorArchitecture == ProcessorArchitecture.None)
+                return assemblyName.FullName;
+            else
+                return assemblyName.FullName + ", ProcessorArchitecture=" + assemblyName.ProcessorArchitecture.ToString().ToLower();
+        }
+
+         public static bool IsFullyQualified(this AssemblyName assemblyName)
+        {
+            return !String.IsNullOrEmpty(assemblyName.Name) && assemblyName.Version != null && assemblyName.CultureInfo != null && assemblyName.GetPublicKeyToken() != null;
+        }
+    }
+}
